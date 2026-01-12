080463823afb2c09400bae8b0eb23577ddeb5e3b
devonbottom <devonbottom797@gmail.com>
2026-01-12 12:47:20 -0600

    Update NumbrixMain.java

--- NumbrixMain.java
+++ NumbrixMain.java
@@ -1,1 +1,15 @@
+import java.io.FileNotFoundException;
 
+public class NumbrixMain
+{
+	/**
+	 * Constructs a Numbrix object and uses it to solve a Numbrix puzzle.
+	 * @throws FileNotFoundException when fileName file does not exist.
+	 */
+	public static void main(String[] args) throws FileNotFoundException
+	{
+		Numbrix puzzle = new Numbrix("Data.txt");
+		System.out.println(puzzle);
+		puzzle.solve();
+	}
+}
